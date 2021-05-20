760e08b3bdea0892b0301b9084f99ab621f68e7b
bear <593788753@qq.com>
2021-05-20 14:16:19 +0800

    单例模式controller 非线程安全

--- src/main/java/com/bear/springmvc5test/controller/common/SingletonDemoController.java
+++ src/main/java/com/bear/springmvc5test/controller/common/SingletonDemoController.java
@@ -1,6 +1,7 @@
 package com.bear.springmvc5test.controller.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,18 +11,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  * @Author: bear
  * @Date: 2021/5/20 9:21 上午
- * @Description: controller 单例模式
+ * @Description: controller 单例模式 @Scope("prototype") 原型模式
  */
 
 @Controller
+//@Scope(value="prototype")
 @RequestMapping("singletonController")
 public class SingletonDemoController {
+
+    private int num = 0;
 
     @Autowired
     private SingletonDemoController singletonDemoController;
 
     @GetMapping("index")
     public void index() {
+        num++;
         System.out.println(singletonDemoController);
+        //第二次访问该方法 num 为2
+        System.out.println(num);
     }
 }
